590408a76c8e2d9d805e296716af187d5360cb0e
93CatDog <vanya101598@gmail.com>
2018-10-31 22:29:11 +0200

    before changes

--- TeachingLoadLib/ExcelIntegration/ExcelPaths.cs
+++ TeachingLoadLib/ExcelIntegration/ExcelPaths.cs
@@ -8,6 +8,7 @@ namespace TeachingLoadCore.ExcelIntegration
     public static class ExcelPaths
     {
         public static String DisciplinesPath = "D:\\pojects\\TeachingLoad\\TeachingLoads\\TeachingLoadLib\\Disciplines.xls";
+        public static String TeachersPath = "D:\\pojects\\TeachingLoad\\TeachingLoads\\TeachingLoadLib\\Teachers.xls";
 
         static ExcelPaths()
         {
--- TeachingLoadLib/ExcelIntegration/ExcelReader.cs
+++ TeachingLoadLib/ExcelIntegration/ExcelReader.cs
@@ -8,53 +8,103 @@ namespace TeachingLoadCore.ExcelIntegration
 {
     public class ExcelReader
     {
-        String path;
-
         private int sheet = 1;
+        Excel.Application excelApp = new Excel.Application();
+        Excel.Workbook excelWorkBook;
+        Excel._Worksheet excelWorkSheet;
 
-        public ExcelReader(String path)
+        public ExcelReader()
         {
-            this.path = path;
+            
         }
 
-        public List<Discipline> ReadSheet()
+        public List<Disciplines> ReadDisciplinesSheet()
         {
-            Excel.Application excelApp = new Excel.Application();
-            Excel.Workbook excelWorkBook = excelApp.Workbooks.Open(path);
-            Excel._Worksheet excelWorkSheet = (Excel.Worksheet)excelWorkBook.Sheets[sheet];
+            excelWorkBook = excelApp.Workbooks.Open(ExcelPaths.DisciplinesPath);
+            excelWorkSheet = (Excel.Worksheet)excelWorkBook.Sheets[sheet];
             Excel.Range excelRange = excelWorkSheet.UsedRange;
 
-            //excelWorkSheet = (Excel.Worksheet)excelWorkBook.Worksheets[Sheet];
-
-            //Worksheet excelSheet = workbook.ActiveSheet;
             excelRange = (Excel.Range)excelWorkSheet.Cells[61, 2];
 
             excelRange = excelWorkSheet.UsedRange;
             int rows = excelRange.Rows.Count;
             int columns = excelRange.Columns.Count;
 
-            List<Discipline> disciplines = new List<Discipline>();
+            List<Disciplines> disciplines = new List<Disciplines>();
 
             for (int i = 1; i < rows + 1; i++)
             {
                 if (excelRange.Cells[i, 1] != null && (excelRange.Cells[i, 1] as Excel.Range).Value2 != null)
                 {
-                    disciplines.Add(new Discipline((string)(excelRange.Cells[i, 1] as Excel.Range).Value2,
+                    disciplines.Add(new Disciplines((string)(excelRange.Cells[i, 1] as Excel.Range).Value2,
                      (string)(excelRange.Cells[i, 2] as Excel.Range).Value2));
                 }
-
-              
-               
             }
-
             excelWorkBook.Close(true, null, null);
             excelApp.Quit();
-
             return disciplines;
-
-            //i++;
-            //j++;
-            ////if (excelWorksheet.Cells[i][j] != null)
         }
+
+        public List<Teachers> ReadTeachersSheet()
+        {
+            excelWorkBook = excelApp.Workbooks.Open(ExcelPaths.TeachersPath);
+            excelWorkSheet = (Excel.Worksheet)excelWorkBook.Sheets[sheet];
+            Excel.Range excelRange = excelWorkSheet.UsedRange;
+
+            excelRange = (Excel.Range)excelWorkSheet.Cells[25, 6];
+
+            excelRange = excelWorkSheet.UsedRange;
+            int rows = excelRange.Rows.Count;
+            int columns = excelRange.Columns.Count;
+
+            List<Teachers> teachers = new List<Teachers>();
+
+            for (int i = 1; i < rows + 1; i++)
+            {
+                if (excelRange.Cells[i, 1] != null && (excelRange.Cells[i, 1] as Excel.Range).Value2 != null)
+                {
+                    teachers.Add(new Teachers((string)(excelRange.Cells[i, 1] as Excel.Range).Value2,
+                     (string)(excelRange.Cells[i, 3] as Excel.Range).Value2,
+                    (string)(excelRange.Cells[i, 4] as Excel.Range).Value2,
+                    (string)(excelRange.Cells[i, 5] as Excel.Range).Value2,
+                    (string)(excelRange.Cells[i, 6] as Excel.Range).Value2));
+                }
+        }
+        excelWorkBook.Close(true, null, null);
+            excelApp.Quit();
+            return teachers;
+        }
+
+
+        //        public List<Groups> ReadGroupsSheet()
+        //        {
+        //            excelWorkBook = excelApp.Workbooks.Open(path);
+        //            excelWorkSheet = (Excel.Worksheet)excelWorkBook.Sheets[sheet];
+        //            Excel.Range excelRange = excelWorkSheet.UsedRange;
+
+        //            excelRange = (Excel.Range)excelWorkSheet.Cells[28, 7];
+
+        //            excelRange = excelWorkSheet.UsedRange;
+        //            int rows = excelRange.Rows.Count;
+        //            int columns = excelRange.Columns.Count;
+
+        //            List<Groups> teachers = new List<Groups>();
+
+        //            for (int i = 1; i < rows + 1; i++)
+        //            {
+        //                if (excelRange.Cells[i, 1] != null && (excelRange.Cells[i, 1] as Excel.Range).Value2 != null)
+        //                {
+        //                    teachers.Add(new Groups((string)(excelRange.Cells[i, 1] as Excel.Range).Value2,
+        //                     (string)(excelRange.Cells[i, 2] as Excel.Range).Value2,
+        //                    (string)(excelRange.Cells[i, 3] as Excel.Range).Value2,
+        //                    (string)(excelRange.Cells[i, 4] as Excel.Range).Value2,
+        //                    (string)(excelRange.Cells[i, 5] as Excel.Range).Value2));
+        //                }
+        //            }
+        //            excelWorkBook.Close(true, null, null);
+        //            excelApp.Quit();
+        //            return groups;
+        //        }
+
     }
 }
